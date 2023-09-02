517462e89bd5278c0b02d0c4a59489bff113157a
go-coding1 <rhtjrwnszyzy@gmail.com>
2023-09-02 20:05:24 +0900

    KotlinSpringBoot First Setting
    KotlinSpringBoot First Setting

---
+++ ClimbPlayGroundBack/src/main/kotlin/com/climbplayground/climbplaygroundback/ClimbPlayGroundBackApplication.kt
@@ -0,0 +1,11 @@
+package com.climbplayground.climbplaygroundback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClimbPlayGroundBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClimbPlayGroundBackApplication>(*args)
+}
